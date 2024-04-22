0f491a6c2b8594fb10f64626d34b071a8a37c4b9
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-22 20:57:33 +0200

    added print message only mailUser is inside mailAccess

--- mail/js/mail.js
+++ mail/js/mail.js
@@ -15,14 +15,24 @@ const mailAccess = [
 ]
 mailAccess.push("giuseppe@Boolean.it")
 
+//Nel ciclo sotto Stampo messaggio sia se nella lista c'è sia se nella lista non c'è
+
 //Ciclo per vedere se la mail dell'utente corrisponde a quella della lista
+// for (let number = 0; number < mailAccess.length; number++) {
+//     //se corrisponde allora può accedere
+//     if (mailUser == mailAccess[number]) {
+//         console.log("L'utente è nella lista e può Accedere", mailAccess[number])
+//     }
+//     //se non corrisponde non può accedere
+//     else if (mailUser != mailAccess[number]) {
+//         console.log("Email Sbagliata")
+//     } 
+// }
+
+// Stampo messaggio solo se nella lista c'è
 for (let number = 0; number < mailAccess.length; number++) {
     //se corrisponde allora può accedere
     if (mailUser == mailAccess[number]) {
         console.log("L'utente è nella lista e può Accedere", mailAccess[number])
     }
-    //se non corrisponde non può accedere
-    else if (mailUser != mailAccess[number]) {
-        console.log("Email Sbagliata")
-    } 
 }
