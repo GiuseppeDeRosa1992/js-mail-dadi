46e6937cb9a7147e8d4b741e43a1599a99ed43e2
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-22 17:44:36 +0200

    added ifand else if winner and print inside page html

--- js/dadi.js
+++ js/dadi.js
@@ -1,11 +1,30 @@
 console.log("ciao giuseppe")
+const div = document.getElementById("dadi")
 
 //GENERO NUMERO PER GIOCATORE
 const userNumber = Math.floor(Math.random() * 6 + 1)
+div.innerHTML += `<div class="user">${userNumber}</div>`
 console.log(userNumber)
+
+
 
 //GENERO UN NUMERO PER IL COMPUTER
 const bothNumber = Math.floor(Math.random() * 6 + 1)
+div.innerHTML += `<div class="both">${bothNumber}</div>`
 console.log(bothNumber)
 
-
+//SE IL NUMERO USER è MAGGIORE DI BOTH ALLORA VINCE
+if (userNumber > bothNumber){
+    div.innerHTML += `<div class="winner">Il Vincitore è User</div>`
+    console.log("win user")
+}
+// SE IL NUMERO è UGUALE è PAREGGIO
+else if (userNumber == bothNumber) {
+    div.innerHTML += `<div class="winner">Pareggio</div>`
+    console.log("draw")
+}
+//ALTRIMENTI VINCE IL BOTH
+else {
+    div.innerHTML += `<div class="winner">Il Vincitore è Both</div>`
+    console.log("win both")
+}
