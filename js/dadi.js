0737f3441cdf117b0341a8240578c6d173b1aafe
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-22 16:32:15 +0200

    added number random user and both

---
+++ js/dadi.js
@@ -0,0 +1,11 @@
+console.log("ciao giuseppe")
+
+//GENERO NUMERO PER GIOCATORE
+const userNumber = Math.floor(Math.random() * 6 + 1)
+console.log(userNumber)
+
+//GENERO UN NUMERO PER IL COMPUTER
+const bothNumber = Math.floor(Math.random() * 6 + 1)
+console.log(bothNumber)
+
+
