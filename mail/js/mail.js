918afb3090f356e149c75779e129c7c6677981ed
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-22 21:12:38 +0200

    added bonus  negative or positive message based on the case inside html and consol.log

--- mail/js/mail.js
+++ mail/js/mail.js
@@ -15,6 +15,8 @@ const mailAccess = [
 ]
 mailAccess.push("giuseppe@Boolean.it")
 
+const div = document.getElementById("mail")
+
 //Nel ciclo sotto Stampo messaggio sia se nella lista c'è sia se nella lista non c'è
 
 //Ciclo per vedere se la mail dell'utente corrisponde a quella della lista
@@ -30,9 +32,22 @@ mailAccess.push("giuseppe@Boolean.it")
 // }
 
 // Stampo messaggio solo se nella lista c'è
+// for (let number = 0; number < mailAccess.length; number++) {
+//     //se corrisponde allora può accedere
+//     if (mailUser == mailAccess[number]) {
+//         console.log("L'utente è nella lista e può Accedere", mailAccess[number])
+//     }
+// }
+
+//Bonus stampo messaggio positivo o negativo in base al caso
 for (let number = 0; number < mailAccess.length; number++) {
     //se corrisponde allora può accedere
     if (mailUser == mailAccess[number]) {
+        div.innerHTML += `<div class="user">L'utente è nella lista e può Accedere</div>`
         console.log("L'utente è nella lista e può Accedere", mailAccess[number])
+    }
+    else {
+        div.innerHTML += `<div class="no-user">Negativo l'utente non è nella lista</div>`
+        console.log("Negativo l'utente non è nella lista")
     }
 }
