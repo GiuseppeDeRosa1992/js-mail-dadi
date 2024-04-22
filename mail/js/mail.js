ab107e91f65bd24b50d14146f9042074da38ead8
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-22 18:18:32 +0200

    addded promt and arrey mailAcces

---
+++ mail/js/mail.js
@@ -0,0 +1,17 @@
+//GENERO PROMT PER CHIEDRE ALL'UTENTE LA LISTA
+const mailUser = prompt("Inserisci la tua mail")
+console.log(mailUser)
+
+//CREO LISTA MAIL
+const mailAccess = [
+    "mail.@prova.it",
+    "giuseppe.@prova.it",
+    "peppe.@prova.it",
+    "peppe@prova.it",
+    "giueppe@prova.it",
+    "mail.@accesso.it",
+    "mail.@provaaccesso.it",
+    "mail.@boolean.it",
+]
+console.log(mailAccess)
+
